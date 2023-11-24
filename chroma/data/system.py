e088ab860e95ecc938752fab350873cb2096c6ec
Chaitanya Joshi <ckjoshi9@gmail.com>
2023-11-24 20:22:09 +0000

    Add missing import

--- chroma/data/system.py
+++ chroma/data/system.py
@@ -20,7 +20,7 @@ import re
 import warnings
 from dataclasses import dataclass
 from functools import partial
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Optional
 
 import numpy as np
 import torch
